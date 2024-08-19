faa32e1b7f9d499897b0ae0bf72e0e377edbc5cd
Steplet <asteberg@list.ru>
2024-08-19 17:31:06 +0300

    test things

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,2 +1,16 @@
-let a = 1
-console.log(a)
+let userData = document.getElementById("greeting")
+
+let btn = document.querySelector("button")
+
+btn.addEventListener("click", function (e) {
+    let inputData = prompt("Put some text!")
+    if (inputData === "a") {
+
+        userData.textContent = "bullshit"
+    }
+    else{
+        userData.textContent = inputData
+    }
+})
+
+let arr = []
